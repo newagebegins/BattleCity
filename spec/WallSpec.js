261dab1b1c423b523dc7b21adea5e28219d27703
newagebegins <newagebegins@gmail.com>
2012-06-08 14:20:12 +1100

    bullet destroys only part of the wall

--- spec/WallSpec.js
+++ spec/WallSpec.js
@@ -9,17 +9,125 @@ describe("Wall", function () {
   it("initial state", function () {
     var eventManager = new EventManager();
     var wall = new Wall(eventManager);
+    
     expect(wall.getWidth()).toEqual(16);
     expect(wall.getHeight()).toEqual(16);
+    
+    expect(wall.isHitLeft()).toBeFalsy();
+    expect(wall.isHitTop()).toBeFalsy();
+    expect(wall.isHitRight()).toBeFalsy();
+    expect(wall.isHitBottom()).toBeFalsy();
   });
   
-  it("should be destroyed when hit by a bullet", function () {
-    var eventManager = new EventManager();
-    var wall = new Wall(eventManager);
-    spyOn(wall, 'destroy');
-    var tank = new Tank(eventManager);
-    var bullet = new Bullet(eventManager, tank);
-    wall.notify({'name': CollisionDetector.Event.COLLISION, 'initiator': bullet, 'sprite': wall})
-    expect(wall.destroy).toHaveBeenCalled();
+  describe("#notify", function () {
+    it("hit by a bullet", function () {
+      var eventManager = new EventManager();
+      var wall = new Wall(eventManager);
+      spyOn(wall, 'hitByBullet');
+      var tank = new Tank(eventManager);
+      var bullet = new Bullet(eventManager, tank);
+      wall.notify({'name': CollisionDetector.Event.COLLISION, 'initiator': bullet, 'sprite': wall});
+      expect(wall.hitByBullet).toHaveBeenCalledWith(bullet);
+    });
+  });
+  
+  describe('#hitByBullet', function () {
+    it("hit left", function () {
+      var eventManager = new EventManager();
+      var wall = new Wall(eventManager);
+      spyOn(wall, 'hitLeft');
+      var tank = new Tank(eventManager);
+      var bullet = new Bullet(eventManager, tank);
+      bullet.setDirection(Sprite.Direction.RIGHT);
+      wall.hitByBullet(bullet);
+      expect(wall.hitLeft).toHaveBeenCalled();
+    });
+    
+    it("hit right", function () {
+      var eventManager = new EventManager();
+      var wall = new Wall(eventManager);
+      spyOn(wall, 'hitRight');
+      var tank = new Tank(eventManager);
+      var bullet = new Bullet(eventManager, tank);
+      bullet.setDirection(Sprite.Direction.LEFT);
+      wall.hitByBullet(bullet);
+      expect(wall.hitRight).toHaveBeenCalled();
+    });
+    
+    it("hit top", function () {
+      var eventManager = new EventManager();
+      var wall = new Wall(eventManager);
+      spyOn(wall, 'hitTop');
+      var tank = new Tank(eventManager);
+      var bullet = new Bullet(eventManager, tank);
+      bullet.setDirection(Sprite.Direction.DOWN);
+      wall.hitByBullet(bullet);
+      expect(wall.hitTop).toHaveBeenCalled();
+    });
+    
+    it("hit bottom", function () {
+      var eventManager = new EventManager();
+      var wall = new Wall(eventManager);
+      spyOn(wall, 'hitBottom');
+      var tank = new Tank(eventManager);
+      var bullet = new Bullet(eventManager, tank);
+      bullet.setDirection(Sprite.Direction.UP);
+      wall.hitByBullet(bullet);
+      expect(wall.hitBottom).toHaveBeenCalled();
+    });
+  });
+  
+  describe("hit multiple times", function () {
+    it("test 1", function () {
+      var eventManager = new EventManager();
+      var wall = new Wall(eventManager);
+      expect(wall.isDestroyed()).toBeFalsy();
+      wall.hitLeft();
+      expect(wall.isDestroyed()).toBeFalsy();
+      wall.hitLeft();
+      expect(wall.isDestroyed()).toBeTruthy();
+    });
+    
+    it("test 2", function () {
+      var eventManager = new EventManager();
+      var wall = new Wall(eventManager);
+      expect(wall.isDestroyed()).toBeFalsy();
+      wall.hitRight();
+      expect(wall.isDestroyed()).toBeFalsy();
+      wall.hitRight();
+      expect(wall.isDestroyed()).toBeTruthy();
+    });
+    
+    it("test 3", function () {
+      var eventManager = new EventManager();
+      var wall = new Wall(eventManager);
+      expect(wall.isDestroyed()).toBeFalsy();
+      wall.hitTop();
+      expect(wall.isDestroyed()).toBeFalsy();
+      wall.hitTop();
+      expect(wall.isDestroyed()).toBeTruthy();
+    });
+    
+    it("test 4", function () {
+      var eventManager = new EventManager();
+      var wall = new Wall(eventManager);
+      expect(wall.isDestroyed()).toBeFalsy();
+      wall.hitBottom();
+      expect(wall.isDestroyed()).toBeFalsy();
+      wall.hitBottom();
+      expect(wall.isDestroyed()).toBeTruthy();
+    });
+    
+    it("test 5", function () {
+      var eventManager = new EventManager();
+      var wall = new Wall(eventManager);
+      expect(wall.isDestroyed()).toBeFalsy();
+      wall.hitTop();
+      expect(wall.isDestroyed()).toBeFalsy();
+      wall.hitRight();
+      expect(wall.isDestroyed()).toBeFalsy();
+      wall.hitBottom();
+      expect(wall.isDestroyed()).toBeTruthy();
+    });
   });
 });
--- src/Wall.js
+++ src/Wall.js
@@ -5,16 +5,103 @@ function Wall(eventManager) {
   
   this._w = 16;
   this._h = 16;
+  
+  this._hitLeft = false;
+  this._hitRight = false;
+  this._hitTop = false;
+  this._hitBottom = false;
 }
 
 Wall.subclass(Sprite);
 
 Wall.prototype.notify = function (event) {
   if (event.name == CollisionDetector.Event.COLLISION && event.initiator instanceof Bullet && event.sprite === this) {
-    this.destroy();
+    this.hitByBullet(event.initiator);
   }
+};
+
+Wall.prototype.hitByBullet = function (bullet) {
+  if (bullet.getDirection() == Sprite.Direction.RIGHT) {
+    this.hitLeft();
+  }
+  else if (bullet.getDirection() == Sprite.Direction.LEFT) {
+    this.hitRight();
+  }
+  else if (bullet.getDirection() == Sprite.Direction.DOWN) {
+    this.hitTop();
+  }
+  else if (bullet.getDirection() == Sprite.Direction.UP) {
+    this.hitBottom();
+  }
+};
+
+Wall.prototype.hitLeft = function () {
+  if (this._hitLeft || this._hitRight) {
+    this.destroy();
+    return;
+  }
+  this._hitLeft = true;
+};
+
+Wall.prototype.isHitLeft = function () {
+  return this._hitLeft;
+};
+
+Wall.prototype.hitRight = function () {
+  if (this._hitRight || this._hitLeft) {
+    this.destroy();
+    return;
+  }
+  this._hitRight = true;
+};
+
+Wall.prototype.isHitRight = function () {
+  return this._hitRight;
+};
+
+Wall.prototype.hitTop = function () {
+  if (this._hitTop || this._hitBottom) {
+    this.destroy();
+    return;
+  }
+  this._hitTop = true;
+};
+
+
+Wall.prototype.isHitTop = function () {
+  return this._hitTop;
+};
+
+Wall.prototype.hitBottom = function () {
+  if (this._hitBottom || this._hitTop) {
+    this.destroy();
+    return;
+  }
+  this._hitBottom = true;
+};
+
+Wall.prototype.isHitBottom = function () {
+  return this._hitBottom;
 };
 
 Wall.prototype.draw = function (ctx) {
   ctx.drawImage(ImageManager.getImage('wall_brick'), this._x, this._y);
+  this._hideDestroyedAreas(ctx);
+};
+
+Wall.prototype._hideDestroyedAreas = function (ctx) {
+  ctx.fillStyle = "black";
+  
+  if (this._hitTop) {
+    ctx.fillRect(this._x, this._y, this._w, this._h / 2);
+  }
+  if (this._hitBottom) {
+    ctx.fillRect(this._x, this._y + this._h / 2, this._w, this._h / 2);
+  }
+  if (this._hitLeft) {
+    ctx.fillRect(this._x, this._y, this._w / 2, this._h);
+  }
+  if (this._hitRight) {
+    ctx.fillRect(this._x + this._w / 2, this._y, this._w / 2, this._h);
+  }
 };
