ba1975397b0a9bebf297b1ce08763ec5d5279f92
newagebegins <newagebegins@gmail.com>
2012-06-11 10:34:46 +1100

    Cursor should move with a delay

---
+++ spec/CursorSpec.js
@@ -0,0 +1,34 @@
+describe("Cursor", function () {
+  it("should move with a delay", function () {
+    var eventManager = new EventManager();
+    var cursor = new Cursor(eventManager);
+    cursor.setMoveDelay(2);
+    cursor.toNormalSpeed();
+    
+    // First move should be without a delay.
+    var position1 = cursor.getPosition();
+    cursor.update();
+    var position2 = cursor.getPosition();
+    expect(position2).not.toEqual(position1);
+    
+    // Then pause.
+    cursor.update();
+    expect(cursor.getPosition()).toEqual(position2);
+    cursor.update();
+    expect(cursor.getPosition()).toEqual(position2);
+    
+    // Move.
+    cursor.update();
+    position1 = cursor.getPosition();
+    expect(position1).not.toEqual(position2);
+    
+    // Pause.
+    cursor.update();
+    expect(cursor.getPosition()).toEqual(position1);
+    
+    // Interrupt the pause. Should move again.
+    cursor.toNormalSpeed();
+    cursor.update();
+    expect(cursor.getPosition()).not.toEqual(position1);
+  });
+});
--- src/Cursor.js
+++ src/Cursor.js
@@ -7,18 +7,39 @@ function Cursor(eventManager) {
   this._h = 32;
   this._normalSpeed = 32;
   this._blinkTimer = new BlinkTimer(12);
+  
+  this._moveDelay = 20;
+  this._moveTimer = 0;
+  this._moved = false;
 }
 
 Cursor.subclass(Sprite);
 
 Cursor.prototype.toNormalSpeed = function () {
   this._speed = this._normalSpeed;
+  this._moved = false;
+};
+
+Cursor.prototype.setMoveDelay = function (delay) {
+  this._moveDelay = delay;
 };
 
 Cursor.prototype.notify = function (event) {
   if (event.name == CollisionDetector.Event.OUT_OF_BOUNDS && event.sprite === this) {
     this.resolveOutOfBounds(event.bounds);
   }
+};
+
+Cursor.prototype.move = function () {
+  if (this._moved) {
+    this._moveTimer++;
+    if (this._moveTimer <= this._moveDelay) {
+      return;
+    }
+  }
+  Sprite.prototype.move.call(this);
+  this._moveTimer = 0;
+  this._moved = true;
 };
 
 Cursor.prototype.updateHook = function () {
