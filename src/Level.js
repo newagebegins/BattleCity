a7c18ab49e24570da6742acae21a1dd0403305a9
newagebegins <newagebegins@gmail.com>
2012-06-13 08:50:30 +1100

    Level: use serializer

--- src/Level.js
+++ src/Level.js
@@ -8,14 +8,9 @@ function Level(eventManager) {
   new BulletFactory(eventManager);
   new ExplosionFactory(eventManager);
 
-  new BrickWall(eventManager).setPosition(new Point(this._x + 32, this._y + 32));
-  new BrickWall(eventManager).setPosition(new Point(this._x + 32, this._y + 48));
-  
-  new BrickWall(eventManager).setPosition(new Point(this._x + 32, this._y + 96));
-  new BrickWall(eventManager).setPosition(new Point(this._x + 32, this._y + 112));
-  
-  new SteelWall(eventManager).setPosition(new Point(this._x + 80, this._y + 48));
-  new SteelWall(eventManager).setPosition(new Point(this._x + 96, this._y + 48));
+  var map = "Base(224,400);BrickWall(208,416);BrickWall(208,400);BrickWall(208,384);BrickWall(224,384);BrickWall(240,384);BrickWall(256,384);BrickWall(256,400);BrickWall(256,416);SteelWall(64,16);SteelWall(80,16);SteelWall(64,32);SteelWall(80,32);SteelWall(64,48);SteelWall(80,48);SteelWall(64,64);SteelWall(80,64);SteelWall(64,80);SteelWall(80,80);SteelWall(64,96);SteelWall(80,96);SteelWall(64,112);SteelWall(80,112);SteelWall(64,128);SteelWall(80,128);SteelWall(96,112);SteelWall(112,112);SteelWall(96,128);SteelWall(112,128);SteelWall(128,112);SteelWall(144,112);SteelWall(128,128);SteelWall(144,128);SteelWall(160,112);SteelWall(176,112);SteelWall(160,128);SteelWall(176,128)";
+  var serializer = new SpriteSerializer(eventManager);
+  serializer.unserializeSprites(map);
 }
 
 Level.subclass(Gamefield);
