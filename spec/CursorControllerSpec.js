bf72bf9c41ca6c0c8840498f3d4d89fdf28d77f7
newagebegins <newagebegins@gmail.com>
2012-06-12 17:28:18 +1100

    CursorController: SPACE pressed - call build()

--- spec/CursorControllerSpec.js
+++ spec/CursorControllerSpec.js
@@ -1,10 +1,20 @@
 describe("CursorController", function () {
-  it("SPACE released - call build()", function () {
+  it("SPACE pressed - call build()", function () {
     var eventManager = new EventManager();
     var cursor = new Cursor(eventManager);
     spyOn(cursor, 'build');
     var cursorController = new CursorController(eventManager, cursor);
+    
+    cursorController.notify({name: Keyboard.Event.KEY_PRESSED, key: Keyboard.Key.SPACE});
+    expect(cursor.build).toHaveBeenCalled();
+    cursor.build.reset();
+    
+    cursorController.notify({name: Keyboard.Event.KEY_PRESSED, key: Keyboard.Key.SPACE});
+    expect(cursor.build).not.toHaveBeenCalled();
+    cursor.build.reset();
+    
     cursorController.notify({name: Keyboard.Event.KEY_RELEASED, key: Keyboard.Key.SPACE});
+    cursorController.notify({name: Keyboard.Event.KEY_PRESSED, key: Keyboard.Key.SPACE});
     expect(cursor.build).toHaveBeenCalled();
   });
 });
--- src/CursorController.js
+++ src/CursorController.js
@@ -1,13 +1,23 @@
 function CursorController(eventManager, cursor) {
   SpriteController.call(this, eventManager, cursor);
+  this._spacePressed = false;
 }
 
 CursorController.subclass(SpriteController);
+
+CursorController.prototype.keyPressed = function (key) {
+  SpriteController.prototype.keyPressed.call(this, key);
+  
+  if (key == Keyboard.Key.SPACE && !this._spacePressed) {
+    this._spacePressed = true;
+    this._sprite.build();
+  }
+};
 
 CursorController.prototype.keyReleased = function (key) {
   SpriteController.prototype.keyReleased.call(this, key);
   
   if (key == Keyboard.Key.SPACE) {
-    this._sprite.build();
+    this._spacePressed = false;
   }
 };
